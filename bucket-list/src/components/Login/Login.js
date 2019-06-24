3407355c5d38112bada86652386a90c9e202256a
Tyler Thompson <tylerthompson@Tylers-MacBook-Pro.local>
2019-06-24 15:15:54 -0400

    started to build out login functionality, added axios call and hooks

--- bucket-list/src/components/Login/Login.js
+++ bucket-list/src/components/Login/Login.js
@@ -1,11 +1,33 @@
 import React, { useState } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-const Login = props => {
-    
+const Login = () => {
+    const [username, updateUsername] = useState('');
+    const [password, updatePassword] = useState('');
+
+    const signIn = creds => {
+        axiosWithAuth()
+            .post('/api/login', creds)
+            .then(res => {
+                console.log(res);
+                localStorage.setITem('token', res.data.payload)
+            })
+            .catch(err => console.log(err))
+    }
+
+    const onSubmit = e => {
+        e.preventDefault();
+        signIn({username, password});
+        updateUsername('');
+        updatePassword('');
+    }
     
     return (
-        <div className='login'>
-            
+        <div onSubmit={onSubmit} className='login'>
+            <form>
+                <input required type='text' name='username' placeholder='Username' />
+                <input required type='text' name='password' placeholder='Password' />
+            </form>
         </div>
     )
 }
--- bucket-list/src/components/utils/axiosWithAuth.js
+++ bucket-list/src/components/utils/axiosWithAuth.js
@@ -7,6 +7,6 @@ export const axiosWithAuth = () => {
         headers: {
             Authorization: token
         },
-        baseURL: 'https://bucket-list-be.herokuapp.com/'
+        baseURL: 'https://bucket-list-be.herokuapp.com'
     })
 }
